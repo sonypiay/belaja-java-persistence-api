f31aa75c88fbec76a1b060c220656879d4d94611
sonypiay <pyscho30@gmail.com>
2025-02-20 01:55:39 +0700

    entity manager

--- src/main/java/programmerzamannow/jpa/entity/Customer.java
+++ src/main/java/programmerzamannow/jpa/entity/Customer.java
@@ -2,8 +2,10 @@ package programmerzamannow.jpa.entity;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 
 @Entity
+@Table(name = "customers")
 public class Customer {
 
     @Id
---
+++ src/test/java/programmerzamannow/jpa/EntityManagerTest.java
@@ -0,0 +1,20 @@
+package programmerzamannow.jpa;
+
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.EntityManagerFactory;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import programmerzamannow.jpa.util.JpaUtil;
+
+public class EntityManagerTest {
+
+    @Test
+    void createEntityManager() {
+        EntityManagerFactory entityManagerFactory = JpaUtil.getEntityManagerFactory();
+        EntityManager entityManager = entityManagerFactory.createEntityManager();
+
+        // operasi database
+        Assertions.assertNotNull(entityManager);
+        entityManager.close();
+    }
+}
