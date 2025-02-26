37b7272f8c2975ace5b3131c904226c03523619e
sonypiay <pyscho30@gmail.com>
2025-02-27 02:38:25 +0700

    join table inheritance

--- src/test/java/programmerzamannow/jpa/InheritanceTest.java
+++ src/test/java/programmerzamannow/jpa/InheritanceTest.java
@@ -5,9 +5,7 @@ import jakarta.persistence.EntityManagerFactory;
 import jakarta.persistence.EntityTransaction;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
-import programmerzamannow.jpa.entity.Employee;
-import programmerzamannow.jpa.entity.Manager;
-import programmerzamannow.jpa.entity.VicePresident;
+import programmerzamannow.jpa.entity.*;
 import programmerzamannow.jpa.util.JpaUtil;
 
 public class InheritanceTest {
@@ -15,7 +13,7 @@ public class InheritanceTest {
     private EntityManagerFactory entityManagerFactory = JpaUtil.getEntityManagerFactory();
 
     @Test
-    void fetch() {
+    void singleTableInsert() {
         EntityManager entityManager = entityManagerFactory.createEntityManager();
         EntityTransaction entityTransaction = entityManager.getTransaction();
         entityTransaction.begin();
@@ -54,6 +52,45 @@ public class InheritanceTest {
         Employee employee = entityManager.find(Employee.class, "budi");
         VicePresident vicePresident = (VicePresident) employee;
         Assertions.assertEquals("Budi Nugroho", vicePresident.getName());
+
+        entityTransaction.commit();
+        entityManager.close();
+        entityManagerFactory.close();
+    }
+
+    @Test
+    void joinTableInsert() {
+        EntityManager entityManager = entityManagerFactory.createEntityManager();
+        EntityTransaction entityTransaction = entityManager.getTransaction();
+        entityTransaction.begin();
+
+        PaymentGopay paymentGopay = new PaymentGopay();
+        paymentGopay.setId("gopay1");
+        paymentGopay.setAmount(100_000L);
+        paymentGopay.setGopayId("0899999");
+        entityManager.persist(paymentGopay);
+
+        PaymentCreditCard paymentCreditCard = new PaymentCreditCard();
+        paymentCreditCard.setId("cc1");
+        paymentCreditCard.setAmount(500_000L);
+        paymentCreditCard.setBank("BCA");
+        paymentCreditCard.setMaskedCard("4555-5555");
+        entityManager.persist(paymentCreditCard);
+
+        entityTransaction.commit();
+        entityManager.close();
+        entityManagerFactory.close();
+    }
+
+    @Test
+    void findJoinTable() {
+        EntityManager entityManager = entityManagerFactory.createEntityManager();
+        EntityTransaction entityTransaction = entityManager.getTransaction();
+        entityTransaction.begin();
+
+        Payment payment = entityManager.find(Payment.class, "gopay1");
+        PaymentGopay paymentGopay = entityManager.find(PaymentGopay.class, "gopay1");
+        PaymentCreditCard paymentCreditCard = entityManager.find(PaymentCreditCard.class, "cc1");
 
         entityTransaction.commit();
         entityManager.close();
